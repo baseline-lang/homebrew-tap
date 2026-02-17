99b4fb0da8a40d0a5fbbb229078a5f1b9d6d0edf
Alastair Dawson <283834+kreek@users.noreply.github.com>
2026-02-17 07:09:34 -0800

    baseline 0.2.0

--- Formula/baseline.rb
+++ Formula/baseline.rb
@@ -2,12 +2,12 @@ class Baseline < Formula
   desc "Strongly typed, effect-based programming language"
   homepage "https://github.com/manifestdocs/baseline"
   license "MIT"
-  version "0.1.0"
+  version "0.2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/manifestdocs/baseline/releases/download/v0.1.0/blc-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "a6d1478b6ab8eb79641b8343f8491262d9c4e97fc1cf67a5f4884d6e453d987a"
+      url "https://github.com/manifestdocs/baseline/releases/download/v0.2.0/blc-v0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "6e79ec8e3b1fdab02ea13574eccf6cd9a6a598c93a87bc72618c5bd4712b407c"
     end
   end
 
